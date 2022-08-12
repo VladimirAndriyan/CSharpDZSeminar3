9b47bbea5668d7f4a187def33faf3ed5e709ecc0
Vladimir <vladimirkristall@yandex.ru>
2022-08-12 17:59:12 +0300

    Add Extra Task using 2 methods

---
+++ TaskExtra2Methods/Program.cs
@@ -0,0 +1,26 @@
+﻿// Написать программу для разворота массива
+// вариант с использованием двух методов
+
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    int index = 0;
+    while (index < count)
+    {
+        Console.Write(arr[index] + " ");
+        index++;
+    }
+}
+void ReverseArray(int[] array)
+{
+    int N = array.Length;
+    for (int i = 0; i < N / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[N - i - 1];
+        array[N - i - 1] = temp;
+    }
+}
+int[] arr = { 1, 8, 3, 6, 5 };
+ReverseArray(arr);
+PrintArray(arr);
