9968aa2635437f03a985dba0ee16da1b3874f573
Vladimir <vladimirkristall@yandex.ru>
2022-08-12 14:55:43 +0300

    Add Extra Task

---
+++ TaskExtra/Program.cs
@@ -0,0 +1,22 @@
+﻿// Написать программу для разворота массива
+
+
+void PrintArray(int[] arr)
+{
+int count = arr.Length;
+int index = 0;
+while(index < count)
+{
+    Console.Write(arr[index] + " ");
+    index ++;
+}
+}
+int[] arr = { 1, 2, 3, 4, 5 };
+int N = arr.Length;
+for (int i = 0; i < N / 2; i++)
+{
+    int temp = arr[i];
+    arr[i] = arr[N - i - 1];
+    arr[N - i - 1] = temp;
+}
+PrintArray(arr);
